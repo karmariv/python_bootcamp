f5733718cfcaf113a78fe6f700886f0967d47572
karmariv <karmariv82@gmail.com>
2023-05-16 19:20:23 -0600

    enhancements

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,6 +1,7 @@
 #def arithmetic_arranger(problems):
 #Define variables
 import sys 
+import re
 
 op = '' #operator
 problems = 0
@@ -10,7 +11,7 @@ line3 = ''
 test = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 
 
-#get the size of the array dddsfdfsdfsd
+#get the size of the array
 problems = len(test)
 
 #Abort if there are more than 5 problems
@@ -20,7 +21,27 @@ if(problems > 5):
     sys.exit()
 
 for prob in test:
+    #Check syntax only numbers and operator '+' and '-' are valid 
+    # Error: Operator must be '+' or '-'   
+    if re.search("[+,-]", prob) == None:
+        print("Error with problem ", prob, ": Operator must be '+' or '-'")
+        #return
+        sys.exit()
+  
     elem = prob.split()
+
+     #Error: Numbers must only contain digits.
+    if elem[0].isdigit() == False or elem[2].isdigit() == False:
+        print("Error with problem ", prob, ": Numbers must only contain digits")
+        sys.exit()
+        #return
+   
+    #Error: Numbers cannot be more than four digits
+    if len(elem[0]) > 4 or len(elem[2]) > 4:
+        print("Error with problem ", prob, ": Numbers cannot be more than four digits")
+        sys.exit()
+        #return
+
 
     #get the length of the greater number in the problem
     elem_length = 0
@@ -31,21 +52,15 @@ for prob in test:
     #get the operator
     op = elem[1]
 
-    #add element to each line
-    elem_1 = elem[0]
-    elem_2 = elem[2]
-
-    if len(elem_1) < elem_length:
-        elem_1 = elem_1.rjust(elem_length + 2)
-
-    
-    if len(elem_2) == elem_length:
-        elem_2 = (op + ' ' + elem_2).rjust(elem_length) 
-
+    #operands
+    operand1 = elem[0].rjust(elem_length + 2)
+    operand2 = op + ' ' + (elem[2]).rjust(elem_length)
+    divider = line3.rjust(elem_length + 2, '-')
+   
     #Build string
-    line1 = line1 + elem_1 + '    '
-    line2 = line2 + elem_2 + '    '
-    line3 = line3 + line3.rjust(elem_length, '-') + '    '
+    line1 = line1 + (operand1).ljust(elem_length + 2 + 4)
+    line2 = line2 + (operand2).ljust(elem_length + 2 + 4)
+    line3 = line3 + divider.ljust(elem_length + 2 + 4)
 
 print(line1)
 print(line2)
