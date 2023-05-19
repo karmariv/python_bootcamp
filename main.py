f122b541e144d2320d383fc390c5590412d6d62b
karmariv <karmariv82@gmail.com>
2023-05-18 18:54:25 -0600

    updated time_calculator.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # This entrypoint file to be used in development. Start by reading README.mdfrom pytest import main
 #
 from arithmetic_arranger import arithmetic_arranger
+from time_calculator import add_time
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+#print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+print(add_time("11:55 PM", "72:12", "wednesday"))
 
 # Run unit tests automatically
 # main(['-vv'])
--- time_calculator.py
+++ time_calculator.py
@@ -1,80 +1,60 @@
-import sys
-import datetime
+def add_time(start, duration, day_of_week=None):
+    #start = "00:00 AM"
+    #duration = "70:00"
+    #day_of_week = "Monday"
+    new_day_of_week = ""
 
-#def add_time(start, duration, day_of_week=None):
-start = "3:00 PM"
-duration = "73:30"
-day_of_week = "Monday"
+    #declare variables
+    week_days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 
-#declare variables
-week_days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+    #Get start time and duration components
+    start_meridian_indicator = start.split()[1]
+    start_hour = int(start.split()[0].split(":")[0])
+    start_minutes = int(start.split()[0].split(":")[1])
+    duration_hour = int(duration.split(":")[0])
+    duration_minutes = int(duration.split(":")[1])
 
-#Get start time and duration components
-start_meridian_indicator = start.split()[1]
-start_hour = int(start.split()[0].split(":")[0])
-start_minutes = int(start.split()[0].split(":")[1])
-duration_hour = int(duration.split(":")[0])
-duration_minutes = int(duration.split(":")[1])
+    #Adjust time if past meridian time
+    if start_meridian_indicator == 'PM':
+        start_hour = start_hour + 12
 
-#Adjust time if past meridian time
-if start_meridian_indicator == 'PM':
-    start_hour = start_hour + 12
+    #Calculate new time
+    new_time_in_minutes = (start_hour * 60) + start_minutes + (duration_hour * 60) + duration_minutes
+   
+    #Get new time hours, minutes
+    new_hours = int(new_time_in_minutes / 60)
+    new_minutes = new_time_in_minutes % 60
 
-#Calculate new time
-new_time_in_minutes = (start_hour * 60) + start_minutes + (duration_hour * 60) + duration_minutes
-print(new_time_in_minutes)
-
-#Get new time hours, minutes
-new_hours = new_time_in_minutes / 60
-new_minutes = new_time_in_minutes % 60
-
-print(new_hours)
-print(new_minutes)
-#if hours is greater than 24 hours then calculate days
-new_days = 0
-if new_hours > 24:
-    new_days = int(int(new_hours) / 24)
-    new_hours = int(new_hours) % 24
-
-#Add AM/PM
-ampm_label = "AM"
-if new_hours > 12:
-    ampm_label = "PM"
-    new_hours = new_hours - 12
+    #if hours is greater than 24 hours then calculate days
+    new_days = 0
+    if new_hours > 24:
+        new_days = int(new_hours / 24)
+        new_hours = new_hours % 24
 
 
-#add next day or ndays label
-if new_days == 1:
-    day_label = '(next day)'
-elif new_days > 1:
-    day_label = str(new_days) + ' days later'
-else:
-    day_label = ''
-
-print(str(new_hours) + ":" + str(new_minutes) + " " + ampm_label + " " + day_label)
+    #Add AM/PM
+    ampm_label = "AM"
+    if new_hours > 12:
+        ampm_label = "PM"
+        new_hours = new_hours - 12
 
 
+    #add next day or ndays label
+    text = ''
+    if new_days == 1:
+        day_label = ' (next day)'
+    elif new_days > 1:
+        day_label = " (" + str(new_days) + ' days later)'
+    else:
+        day_label = ''
 
-#print(start_time_in_minutes)
-#check if a valid week_day_value was received
-#if day_of_week == None:
-#    day_of_week = datetime.datetime.now().strftime("%A")
-#    print(day_of_week)
-#else:
-#    if day_of_week.lower() in week_days:
-#        print("wwooooow")
-#    else:
-#        print("ERROR: Invalid day of Week, value must be any of the following: ", week_days)
-#        #sys.exit()
+    #getting the day of the week if provided
+    if day_of_week != None:
+        offset = (week_days.index(day_of_week.lower()) + new_days) % 7
+        new_day_of_week = week_days[offset]
+        day_label = " " + new_day_of_week.capitalize() + day_label
 
-#Build date
-#year = 
-#month = 
-#day = 
+    output =  str(new_hours) + ":" + str(new_minutes).zfill(2) + " " + ampm_label + day_label
 
-    
+    return output
 
-
-
-
-#    return new_time
